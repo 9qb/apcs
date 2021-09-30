213b976b978105f32d533133fb87272f687fcb4b
Brian Li <61071032+bli31@users.noreply.github.com>
2021-09-30 12:14:09 -0400

    Create Greet.java

---
+++ 08_hellofxn/Greet.java
@@ -0,0 +1,10 @@
+// Brian Li
+// APCS
+// HW07 -- Basic printed greetings!
+// 2021-09-29
+
+public class Greet{
+  public static void main(String[] args){
+    System.out.println("No hablo queso");
+  }
+}
