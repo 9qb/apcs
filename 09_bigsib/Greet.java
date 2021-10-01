b9335a7e9218ef6fa8a49c5167aa6f83da80ea59
Brian Li <61071032+bli31@users.noreply.github.com>
2021-10-01 12:01:34 -0400

    Create Greet.java

---
+++ 09_bigsib/Greet.java
@@ -0,0 +1,24 @@
+// Giant Lotus -- Brian Li, Ryan Lau, Weichen Liu
+// APCS
+// HW08 -- Basic printed greetings!
+// 2021-09-30
+
+/*
+DISCOVERIES
+ 0. If you update Greet.java, you have to re-javac Greet.java so that java Greet runs the updated Greet.java.
+UNRESOLVED QUESTIONS
+ 0. Are we supposed to randomly pick 1 of the 3 hardcoded persons to greet?
+*/
+
+public class Greet{
+
+  public static void greet(String str){
+    System.out.println("Why, hello there, " + str + ". How do you do?");
+  }
+
+  public static void main(String[] args){
+    greet("Test");
+    greet("Foo");
+    greet("Moo");
+  }
+}
