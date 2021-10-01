e5a7e0f9d503110cf93f9984eea87be02905453a
Brian Li <61071032+bli31@users.noreply.github.com>
2021-10-01 18:17:13 -0400

    Update Greet.java

--- 09_bigsib/Greet.java
+++ 09_bigsib/Greet.java
@@ -7,6 +7,7 @@
 DISCOVERIES
  0. You can't compile Greet.java without first compiling BigSib.java, because the code of Greet.java refers to elements of BigSib. If BigSib.class is not present, compiling Greet.java will also compile BigSib.java.
 UNRESOLVED QUESTIONS
+ 1. Since Greet.java and BigSib.java are located within the same directory, no import is needed. You can utilize methods from different classes in the form of <class name>.<method name>
 */
 
 public class Greet {
