9e7e719535606e1378ff5fc3654276bdead84c5f
Brian Li <61071032+bli31@users.noreply.github.com>
2021-10-04 16:39:42 -0400

    Update BigSib.java

--- 10_bigsib/BigSib.java
+++ 10_bigsib/BigSib.java
@@ -24,8 +24,8 @@ UNRESOLVED QUESTIONS
 
 public class BigSib{
 
-  public static void greet(String str){
-    System.out.println("Why, hello there, " + str + ". How do you do?");
+  public static String greet(String str){
+    return("Why, hello there, " + str + ". How do you do?");
     
   }
 }
