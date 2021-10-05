f73cb8b946bae2a6b3686b00ccd016fb9ef3bb07
Brian Li <61071032+bli31@users.noreply.github.com>
2021-10-05 19:28:43 -0400

    Update Greet.java

--- 12_bigsib/Greet.java
+++ 12_bigsib/Greet.java
@@ -4,11 +4,11 @@
 // 2021-10-05
 
 /*
-DISCOVERIES
- 0. When we create variable richard of type BigSib, rihard is able to use all of the methods associated with the class BigSib, such as setHelloMsg and greet, in the form variableName/className.method().
+DISCO
+ 0. When we create variable richard of type BigSib, richard is able to use all of the methods associated with the class BigSib, such as setHelloMsg and greet, in the form variableName/className.method().
  1. A static method is unable to reference instance variables. That is, the method is incapable of referencing variables outside of its own class. To remedy this, remove static from the class construction.
     
-UNRESOLVED QUESTIONS
+QCC
  0. When creating a variable of a class type, what happens if you put something within the parentheses of the class (e.g. BigSib richard = new BigSib();)? What does that affect?
 */
 
