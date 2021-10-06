d4ac477c16494e43c41ff8f4606f7ec268dc270a
bli31 <brianlisayshi@gmail.com>
2021-10-06 15:57:19 -0400

    init

---
+++ 13_hatch/BigSib.java
@@ -0,0 +1,26 @@
+// Dynamic Duo of Destiny -- Brian Li, Kaitlin Ho, Robert, Apple
+// APCS
+// HW12 -- On Elder Individuality and the Elimination of Radio Fuzz
+// 2021-10-05
+
+/*
+DISCO
+ 0. When we create variable richard of type BigSib, richard is able to use all of the methods associated with the class BigSib, such as setHelloMsg and greet, in the form variableName/className.method().
+ 1. A static method is unable to reference instance variables. That is, the method is incapable of referencing variables outside of its own class. To remedy this, remove static from the class construction.
+    
+QCC
+ 0. When creating a variable of a class type, what happens if you put something within the parentheses of the class (e.g. BigSib richard = new BigSib();)? What does that affect?
+*/
+
+public class BigSib{
+ String helloMsg;
+ 
+  public String greet(String name){
+    return(helloMsg + " " + name);
+  }
+ 
+  public void setHelloMsg(String msg){
+   helloMsg = msg;
+  }
+ 
+}
---
+++ 13_hatch/Greet.java
@@ -0,0 +1,45 @@
+// Dynamic Duo of Destiny -- Brian Li, Kaitlin Ho, Robert, Apple
+// APCS
+// HW12 -- On Elder Individuality and the Elimination of Radio Fuzz
+// 2021-10-05
+
+/*
+DISCO
+ 0. When we create variable richard of type BigSib, richard is able to use all of the methods associated with the class BigSib, such as setHelloMsg and greet, in the form variableName/className.method().
+ 1. A static method is unable to reference instance variables. That is, the method is incapable of referencing variables outside of its own class. To remedy this, remove static from the class construction.
+    
+QCC
+ 0. When creating a variable of a class type, what happens if you put something within the parentheses of the class (e.g. BigSib richard = new BigSib();)? What does that affect?
+*/
+
+public class Greet {
+  public static void main( String[] args ) {
+    String greeting;
+    BigSib richard = new BigSib();
+
+    // Word up freshman
+    richard.setHelloMsg("Word up");
+
+    greeting = richard.greet("freshman");
+    System.out.println(greeting);
+   
+    // Salutations Dr. Spaceman
+    richard.setHelloMsg("Salutations");
+   
+    greeting = richard.greet("Dr. Spaceman");
+    System.out.println(greeting);
+   
+    // Hey ya King Fooey
+    richard.setHelloMsg("Hey ya");
+   
+    greeting = richard.greet("King Fooey");
+    System.out.println(greeting);
+   
+    // Sup mom
+    richard.setHelloMsg("Sup");
+   
+    greeting = richard.greet("mom");
+    System.out.println(greeting);
+
+  } //end main()
+} //end Greet
