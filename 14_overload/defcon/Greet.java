a3802a749dd09b98930cb8df3f0898df7596adf5
Brian Li <61071032+bli31@users.noreply.github.com>
2021-10-07 19:38:49 -0400

    Update Greet.java

--- 14_overload/defcon/Greet.java
+++ 14_overload/defcon/Greet.java
@@ -15,18 +15,17 @@ QCC
 public class Greet {
   public static void main( String[] args ) {
     String greeting;
-    BigSib richard = new BigSib("Word up");
-    BigSib grizz = new BigSib("Salutations");
-    BigSib dotCom = new BigSib("Hey ya");
-    BigSib tracy = new BigSib("Sup");
-
+    BigSib richard = new BigSib();
+    BigSib grizz = new BigSib();
+    BigSib dotCom = new BigSib();
+    BigSib tracy = new BigSib();
     greeting = richard.greet("freshman");
     System.out.println(greeting);
-    greeting = richard.greet("Dr. Spaceman");
+    greeting = tracy.greet("Dr. Spaceman");
     System.out.println(greeting);
-    greeting = richard.greet("King Fooey");
+    greeting = grizz.greet("King Fooey");
     System.out.println(greeting);
-    greeting = richard.greet("mom");
+    greeting = dotCom.greet("mom");
     System.out.println(greeting);
   } //end main()
 } //end Greet
