c7ba7c86403f3dc669081c9a78e15431ef9470ec
Brian Li <61071032+bli31@users.noreply.github.com>
2021-10-07 19:44:49 -0400

    Update BigSib.java

--- 14_overload/defcon/BigSib.java
+++ 14_overload/defcon/BigSib.java
@@ -5,11 +5,10 @@
 
 /*
 DISCO
- 0. When assigning new <ClassName>() to the same variable again, it resets the class instance to its original state. 
- 1. A constructor can accept arguments, similar to a method. The arguments can be supplied when initializing an instance of a class within the parentheses.
+ 0. When we overload a method/constructor, we are able to supply different datatypes and/or a different number of arguments to methods of the same name. The method that is invoked depends on which method's parameters are satisfied by the arguments.
     
 QCC
- 0. Why is it necessary for the constructor to be public? And why does it have to be the same name of the class?
+ 0. Is it possible to overload main()? What would happen in that scenario?
 */
 
 public class BigSib{
