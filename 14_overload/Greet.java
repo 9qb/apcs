d6e1aa57b22e196c852ca229c8442a46b37163c9
Brian Li <61071032+bli31@users.noreply.github.com>
2021-10-07 19:06:50 -0400

    update

--- 14_overload/Greet.java
+++ 14_overload/Greet.java
@@ -15,30 +15,19 @@ QCC
 public class Greet {
   public static void main( String[] args ) {
     String greeting;
-
-    // Word up freshman
     BigSib richard = new BigSib("Word up");
-   
+    BigSib grizz = new BigSib("Salutations");
+    BigSib dotCom = new BigSib("Hey ya");
+    BigSib tracy = new BigSib("Sup");
+
     greeting = richard.greet("freshman");
     System.out.println(greeting);
-   
-    // Salutations Dr. Spaceman
-    richard = new BigSib("Salutations");
-   
     greeting = richard.greet("Dr. Spaceman");
     System.out.println(greeting);
-   
-    // Hey ya King Fooey
-    richard = new BigSib("Hey ya");
-   
     greeting = richard.greet("King Fooey");
     System.out.println(greeting);
-   
-    // Sup mom
-    richard = new BigSib("Sup");
-   
     greeting = richard.greet("mom");
     System.out.println(greeting);
-
   } //end main()
 } //end Greet
+
