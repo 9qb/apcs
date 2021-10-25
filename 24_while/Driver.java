6f4ef5ed768b0a9881323ecdd47679b2ce4df512
Brian Li <61071032+bli31@users.noreply.github.com>
2021-10-25 18:39:33 -0400

    change conditional

--- 24_while/Driver.java
+++ 24_while/Driver.java
@@ -56,7 +56,7 @@ public class Driver {
       x = 100;
       y = 66165;
       
-      while (x < yours.getFlipCtr() && y < matchCtr && (matchCtr >= 65536 && y % 2005 == 0)){
+      while (x > yours.getFlipCtr() || y > matchCtr || (matchCtr >= 65536 && y % 2005 == 0)){
         if ( yours.equals(wayne) ) {
           // System.out.println( "Matchee matchee!" );
           matchCtr++;
