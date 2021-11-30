0c8434b34f73d16cc7515b3f18f2f207bc0d4dae
bli31 <bli31@stuy.edu>
2021-11-30 12:11:47 -0500

    fix

--- PassBy.java
+++ PassBy.java
@@ -1,14 +1,24 @@
 public class PassBy{
+  public static int changeInt(int arg){
+    arg += 1;
+    return arg;
+  }
+
+  public static String concatenate(String arg){
+    arg += "hello";
+    return arg;
+  }
+
   public static void main(String[] args){
     int test = 10;
     int refTest = test;
-    refTest += 1;
+    changeInt(refTest);
     System.out.println(test == refTest); // if true, then Java is pass-by-reference.
                                          // if false, then Java is pass-by-value.
 
     String testString = "hi";
     String anotherString = testString;
-    anotherString += "hello";
+    concatenate(anotherString);
     System.out.println(testString.equals(anotherString)); // if true, then Java is pass-by-reference.
                                                           // if false, then Java is pass-by-value.
   }
