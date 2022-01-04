ae9943e998188d16e31854bf4c238aa1030ff3e4
Brian Li <bli31@stuy.edu>
2022-01-04 18:53:09 -0500

    upd

--- 52_selectionsort/SelectionSort.java
+++ 52_selectionsort/SelectionSort.java
@@ -1,25 +1,33 @@
-// Clyde "Thluffy" Sinclair
-// APCS pd0
+// Peanut Butter and Nutella - Prattay Dey + Winnie, Brian Li + Robert, Nafiz Labib + Martha
+// APCS pd6
 // HW52 -- implementing selection sort
 // 2022-01-05w
-// time spent:  hrs
+// time spent: 0.7 hrs
 
 /******************************
  *   class SelectionSort -- implements SelectionSort algorithm
  *
  * ALGO:
+ * Find the largest value within the dataset and swap its place with the largest
+ * index of the dataset. Find the next largest value within the dataset and swap
+ * its place with the next largest index of the dataset. This is repeated until
+ * the next largest value and the next largest index are at the same position.
  *
  * DISCO
+ * A method that returns a value can serve as an argument, while additionally
+ * performing everything else instructed inside that method's invocation.
  *
  * QCC
  * q0: How many passes to sort n elements?
- * a0:
+ * a0: n-1 passes (starting count from 1)
  * q1: What do you know after pass p?
- * a1:
+ * a1: The element p-1 indices from the right is in its correct place.
+ *     (assuming p starts count from 1)
  * q2: How do you know if sorted?
- * a2:
+ * a2: maxPos is equal to the largest index in the dataset.
  * q3: What does a pass boil down to?
- * a3:
+ * a3: A pass boils down to searching for the index possessing the highest value
+ *     and swapping that with the next available index from the right.
  ******************************/
 
 
@@ -67,24 +75,32 @@ public class SelectionSort
     //maxPos will point to position of SELECTION (greatest value)
     int maxPos;
 
-    for(int pass = data.size(); pass > 0; pass--) {
-      System.out.println( "\nbegin pass " + (data.size()-pass) );//diag
+    // iterates until pass is 0
+    for(int pass = data.size() - 1; pass > 0; pass--) {
+      // System.out.println( "\nbegin pass " + (data.size()-pass) );//diag
+      maxPos = 0;
 
+      // iterates until i <= pass, because the value at index pass+1 will already
+      // have the correct value in it, as a result of the previous iteration of the
+      // above for loop.
+      for(int i = 0; i <= pass; i++) {
 
-      for(int i = 0; i + 1 < pass; i++) {
-        if ((data.get(i)).compareTo(data.get(i+1)) < 0){
-          maxPos = i+1;
+        // if the value at index maxPos < value at index i, update maxPos
+        if ((data.get(maxPos)).compareTo(data.get(i)) < 0){
+          maxPos = i;
         }
 
-        System.out.println( "maxPos: " + maxPos );//diag
-        System.out.println( data );//diag
-
+        // System.out.println( "maxPos: " + maxPos );//diag
+        // System.out.println( data );//diag
 
       }
 
-      
+      // swap
+      Comparable temp = data.get(pass);
+      data.set(pass, data.get(maxPos));
+      data.set(maxPos, temp);
 
-      System.out.println( "after swap: " +  data );//diag
+      // System.out.println( "after swap: " +  data );//diag
     }
   }//end selectionSort
 
@@ -111,7 +127,6 @@ public class SelectionSort
   public static void main( String [] args )
   {
 
-    /*===============for VOID methods=============
     ArrayList glen = new ArrayList<Integer>();
     glen.add(7);
     glen.add(1);
@@ -126,28 +141,28 @@ public class SelectionSort
     System.out.println( "ArrayList coco before sorting:\n" + coco );
     selectionSortV(coco);
     System.out.println( "ArrayList coco after sorting:\n" + coco );
+    /*===============for VOID methods=============
       ============================================*/
 
-    /*==========for AL-returning methods==========
-      ArrayList glen = new ArrayList<Integer>();
-      glen.add(7);
-      glen.add(1);
-      glen.add(5);
-      glen.add(12);
-      glen.add(3);
-      System.out.println( "ArrayList glen before sorting:\n" + glen );
-      ArrayList glenSorted = selectionSort( glen );
-      System.out.println( "sorted version of ArrayList glen:\n"
-      + glenSorted );
-      System.out.println( "ArrayList glen after sorting:\n" + glen );
+      ArrayList glen2 = new ArrayList<Integer>();
+      glen2.add(7);
+      glen2.add(1);
+      glen2.add(5);
+      glen2.add(12);
+      glen2.add(3);
+      System.out.println( "ArrayList glen2 before sorting:\n" + glen2 );
+      ArrayList glen2Sorted = selectionSort( glen2 );
+      System.out.println( "sorted version of ArrayList glen2:\n"
+      + glen2Sorted );
+      System.out.println( "ArrayList glen2 after sorting:\n" + glen2 );
 
-      ArrayList coco = populate( 10, 1, 1000 );
-      System.out.println( "ArrayList coco before sorting:\n" + coco );
-      ArrayList cocoSorted = selectionSort( coco );
-      System.out.println( "sorted version of ArrayList coco:\n"
-      + cocoSorted );
-      System.out.println( "ArrayList coco after sorting:\n" + coco );
-      System.out.println( coco );
+      ArrayList coco2 = populate( 10, 1, 1000 );
+      System.out.println( "ArrayList coco2 before sorting:\n" + coco2 );
+      ArrayList coco2Sorted = selectionSort( coco2 );
+      System.out.println( "sorted version of ArrayList coco2:\n"
+      + coco2Sorted );
+      System.out.println( "ArrayList coco2 after sorting:\n" + coco2 );
+      /*==========for AL-returning methods==========
       ============================================*/
 
   }//end main
