d2628734ddf61cde29dfd713b2695a2ed3103b16
Brian Li <bli31@stuy.edu>
2022-01-12 20:17:26 -0500

    updated

--- cobo/magpie/activity2/Magpie2.java
+++ cobo/magpie/activity2/Magpie2.java
@@ -1,3 +1,17 @@
+// Peanut Butter and Nutella - Prattay Dey + Winnie, Brian Li + Robert, Nafiz Labib + Martha
+// APCS pd6
+// HW56 -- Turing Training Wheels
+// 2022-01-12
+// time spent: 1.0 hours
+
+/*
+DISCO:
+- The else-if statement that is utilized is the one with greater priority.
+- trim() gets rid of leading and lagging whitespace.
+QCC:
+- Is there a way to refactor the code so that it isn't so "long"?
+*/
+
 /**
  * A program to carry on conversations with a human user.
  * This is the initial version that:
@@ -10,6 +24,11 @@
  * @author Laurie White
  * @version April 2012
  */
+
+ // Q1: when a keyword is included in another word, it generates a response
+ // corresponding to the keyword, resulting in a response that doesn't make
+ // sense.
+
 public class Magpie2
 {
 	/**
@@ -30,6 +49,9 @@ public class Magpie2
 	 */
 	public String getResponse(String statement)
 	{
+		// responses are prioritized by ranking. the response is based on
+		// which of the if statements or if-else statements is satisfied first.
+
 		String response = "";
 		if (statement.indexOf("no") >= 0)
 		{
@@ -53,6 +75,18 @@ public class Magpie2
 		}
 		else if ((statement.trim()).equals("")){
 			response = "Say something, please.";
+		}
+		else if (statement.indexOf("like") >= 0)
+		{
+			response = "I like it, too.";
+		}
+		else if (statement.indexOf("sad") >= 0)
+		{
+			response = "Smile, life is beautiful.";
+		}
+		else if (statement.indexOf("happy") >= 0)
+		{
+			response = "Being happy is great.";
 		}
 		else
 		{
