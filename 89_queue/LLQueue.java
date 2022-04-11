0f1376a9069156be169d1b7372144d440e9fbd9e
Brian Li <bli31@stuy.edu>
2022-04-11 18:12:06 -0400

    d

--- 89_queue/LLQueue.java
+++ 89_queue/LLQueue.java
@@ -1,3 +1,18 @@
+/*
+(P)BNJ -- Nakib Abedin + Jefford Shau + Brian Li
+APCS pd07
+HW89 --  Queue Two Ways
+2022-04-11
+time spent: 0.1 hr + class time
+
+DISCO
+1) Passing through the functionality of ArrayList and LinkedList to Queue makes coding the Wrapper Class for ArrayList and LinkedList much easier.
+2) The encapsulation diagram helped us identify the time complexity of each method. 
+
+QCC:
+1) How can we make AlQueue have constant runtime for both enqueue and dequeue?
+*/
+
 import java.util.LinkedList;
 
 public class LLQueue<QUASAR> implements Queue<QUASAR>{
